61467ae1b0e73dbeeae782d5fc5dc6a1a9b76c83
Terry Mafura <maffsojah1@gmail.com>
2022-06-22 16:40:14 +0200

    Initial Commit

---
+++ src/main/java/com/mywork/javaassessment/JavaAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.mywork.javaassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAssessmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaAssessmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mywork/javaassessment/JavaAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mywork.javaassessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAssessmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
